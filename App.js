2b1b851555259dbdc517bb3d5cc7b335eca88555
Alberto626 <47710569+Alberto626@users.noreply.github.com>
2021-04-22 20:21:23 -0700

    test

--- App.js
+++ App.js
@@ -1,21 +1,94 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import React, { useState } from 'react';
+import { Button, FlatList, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator(); // helps with screens
+
+const App = props => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name = "Home" component={Home}/>
+        <Stack.Screen name = "Description" component={Description}/>
+     </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+const Home = props => {
+  const [userInput, setUserInput] = useState('')
+  const [data, setData] = useState([])
+  const [renderList, setRenderList] = useState(false)
+  const renderItem = item => {
+    console.log(item)
+    return (
+      <Text>{item.item.Title}</Text>
+    )
+  }
+  return(
+    <SafeAreaView>
+      <TextInput
+        placeholder = "Please Enter a Movie"
+        onChangeText = {setUserInput}
+        value = {userInput}
+      />
+      <Button
+        title = "Search"
+        onPress = {() => {apiCall(userInput, data); setRenderList(!renderList)}}
+      />
+      <Button
+        title = "press to print"
+        onPress = {() => console.log(data)}
+      />
+      <FlatList
+        data = {data}
+        renderItem = {renderItem}
+        keyExtractor = {item => item.imdbID}
+        ListEmptyComponent = {empty}
+        extraData = {renderList}
+      />
+    </SafeAreaView>
+  );
+}
+const empty = () => {
+  return (
+    <Text>There is nothing here, Search something</Text>
+  )
+}
+const apiCall = async (userInput, data) =>  {
+  let search;
+  if(userInput !== '') { // do the api call
+    while(data.length > 0) { // empty the array when creating a new search
+      data.pop();
+    }
+    search = await fetch(`http://www.omdbapi.com/?apikey=33014428&s=${userInput}`)
+    .then(response => response.json()) // response is a promise and to access the text, we must convert to a json
+      .then(data => { // data is the response.json, does not refer to the parameter
+        return data.Search; // returns the results of array of objects of the movies
+        }
+      )
+    let i = 0
+    for(i; i < search.length; i++ ) { // this is to create the iterate through search and create an object for data
+      const movie = { // this to recreate the data but change the poster if N/A appears
+        Poster: search[i].Poster.localeCompare(`N/A`) === 0 ? `https://images.uncyclomedia.co/uncyclopedia/en/thumb/0/01/DramaticQuestionMark.png/300px-DramaticQuestionMark.png`: search[i].Poster,
+        Title: search[i].Title,
+        Type: search[i].Type,
+        Year: search[i].Year,
+        imdbID: search[i].imdbID
+      }
+      data.push(movie)
+    }
+  }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+}
+const Description = props => {
+  return(
+    <SafeAreaView>
+      <Text>Hello Description</Text>
+    </SafeAreaView>
+  )
+}
+//() => props.navigation.navigate("Description")
+export default App
+
