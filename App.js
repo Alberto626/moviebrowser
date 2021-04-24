43a1e4890491eb5dae2bc290ee218fdb86626553
Alberto626 <47710569+Alberto626@users.noreply.github.com>
2021-04-24 12:46:28 -0700

    finally did it

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 
-import React, { useState } from 'react';
-import { Button, FlatList, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Button, FlatList, SafeAreaView, StyleSheet, Text, TextInput, View,Image, Alert } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -21,9 +21,24 @@ const Home = props => {
   const [data, setData] = useState([])
   const [renderList, setRenderList] = useState(false)
   const renderItem = item => {
-    console.log(item)
     return (
-      <Text>{item.item.Title}</Text>
+      <SafeAreaView style = {{flexDirection: 'row'}}>
+        <Image
+        style = {styles.logo}
+        source ={{ uri: item.item.Poster }}
+        />
+        <SafeAreaView style = {{flexDirection : 'column'}}>
+        <Text style = {{fontWeight: `bold`}}>{item.item.Title}</Text>
+        <Text>{item.item.Type}</Text>
+        <Text>From: {item.item.Year}</Text>
+        <Text>ID: {item.item.imdbID}</Text>
+        <Button
+          style = {styles.buttonStyle}
+          title = "Click For More"
+          onPress = {() => props.navigation.navigate("Description", {imdbID: item.item.imdbID, poster: item.item.Poster})}
+        />
+        </SafeAreaView>
+      </SafeAreaView>
     )
   }
   return(
@@ -32,14 +47,11 @@ const Home = props => {
         placeholder = "Please Enter a Movie"
         onChangeText = {setUserInput}
         value = {userInput}
+        style = {{height: 40, borderWidth: 1}}
       />
       <Button
         title = "Search"
-        onPress = {() => {apiCall(userInput, data); setRenderList(!renderList)}}
-      />
-      <Button
-        title = "press to print"
-        onPress = {() => console.log(data)}
+        onPress = {() => apiCall(userInput, data, setRenderList)}
       />
       <FlatList
         data = {data}
@@ -47,6 +59,7 @@ const Home = props => {
         keyExtractor = {item => item.imdbID}
         ListEmptyComponent = {empty}
         extraData = {renderList}
+        contentContainerStyle = {{ paddingBottom : 100}}
       />
     </SafeAreaView>
   );
@@ -56,39 +69,78 @@ const empty = () => {
     <Text>There is nothing here, Search something</Text>
   )
 }
-const apiCall = async (userInput, data) =>  {
+const apiCall = async (userInput, data, setRenderList) =>  {
   let search;
   if(userInput !== '') { // do the api call
-    while(data.length > 0) { // empty the array when creating a new search
-      data.pop();
-    }
     search = await fetch(`http://www.omdbapi.com/?apikey=33014428&s=${userInput}`)
     .then(response => response.json()) // response is a promise and to access the text, we must convert to a json
       .then(data => { // data is the response.json, does not refer to the parameter
-        return data.Search; // returns the results of array of objects of the movies
+        console.log(data); return data.Search; // returns the results of array of objects of the movies
         }
       )
-    let i = 0
-    for(i; i < search.length; i++ ) { // this is to create the iterate through search and create an object for data
-      const movie = { // this to recreate the data but change the poster if N/A appears
-        Poster: search[i].Poster.localeCompare(`N/A`) === 0 ? `https://images.uncyclomedia.co/uncyclopedia/en/thumb/0/01/DramaticQuestionMark.png/300px-DramaticQuestionMark.png`: search[i].Poster,
-        Title: search[i].Title,
-        Type: search[i].Type,
-        Year: search[i].Year,
-        imdbID: search[i].imdbID
+      if(search) { // if the search is valid
+        while(data.length > 0) { // empty the array when creating a new search
+          data.pop();
+        }
+        let i = 0
+        for(i; i < search.length; i++ ) { // this is to create the iterate through search and create an object for data
+          const movie = { // this to recreate the data but change the poster if N/A appears
+            Poster: search[i].Poster.localeCompare(`N/A`) === 0 ? `https://images.uncyclomedia.co/uncyclopedia/en/thumb/0/01/DramaticQuestionMark.png/300px-DramaticQuestionMark.png`: search[i].Poster,
+            Title: search[i].Title,
+            Type: search[i].Type,
+            Year: search[i].Year,
+            imdbID: search[i].imdbID
+          }
+          data.push(movie)
+          setRenderList(true) // These are to rerender the flatList
+          setRenderList(false)
+        }
       }
-      data.push(movie)
-    }
+      else{
+        Alert.alert(`Movies not found`)
+      }
   }
 
 }
 const Description = props => {
+  const [movie, setMovie] = useState(new Object())
+  useEffect(() => {
+    const searchByID = async(ID) => {
+      let search = await fetch(`http://www.omdbapi.com/?apikey=33014428&i=${ID}`)
+      .then(response => response.json())
+      setMovie(search)
+    }
+    searchByID(props.route.params.imdbID)
+
+  })
   return(
-    <SafeAreaView>
-      <Text>Hello Description</Text>
+    <SafeAreaView style = {{alignItems: `center`}}>
+      <Text style = {{fontWeight: `bold`}}>{movie.Title}</Text>
+      <Image
+        style = {{width: 300, height: 300}}
+        source ={ { uri: props.route.params.poster }}
+      />
+      <Text>Genre: {movie.Genre}</Text>
+      <Text>Actors: {movie.Actors}</Text>
+      <Text>Year: {movie.Year} Rated: {movie.Rated} Released: {movie.Released}</Text>
+      <Text>Director: {movie.Director} Writer: {movie.Writer}</Text>
+      
+      <Text style = {{fontWeight: `bold`,fontSize: 20}}>Plot: {movie.Plot}</Text>
     </SafeAreaView>
   )
 }
+const styles = StyleSheet.create({
+  logo: {
+    width: 125,
+    height: 125
+  },
+  buttonStyle: {
+    height: 20,
+    marginTop: 20,
+    alignItems: 'center'
+
+  }
+})
 //() => props.navigation.navigate("Description")
 export default App
 
